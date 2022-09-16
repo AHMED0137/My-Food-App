419485ba257f66051095a6968484b12ae2ba5686
AHMED0137 <108218421+Mohsin-Israr1@users.noreply.github.com>
2022-09-16 21:46:50 +0500

    User

---
+++ Frontend/User/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Navbutton from "./components/navigation/Navbutton";
+function App() {
+  return (
+    <>
+      <Navbutton />
+    </>
+  );
+}
+
+export default App;
